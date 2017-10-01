eab3aa623b45f299e1df18caad524095c9fc59e5
Wykks <wykks@users.noreply.github.com>
2017-10-01 19:30:25 +0200

    fix: workaround rollup issue

--- src/app/lib/map/map.service.ts
+++ src/app/lib/map/map.service.ts
@@ -11,8 +11,27 @@ export class MapService {
     options: MapboxGl.MapboxOptions,
     customMapboxApiUrl?: string
   ) {
-    (<any>MapboxGl).accessToken = accessToken;
-    (<any>MapboxGl).config.customMapboxApiUrl = customMapboxApiUrl;
+    // Workaround rollup issue
+    this.assign(MapboxGl, 'accessToken', accessToken);
+    this.assign(MapboxGl, 'config.customMapboxApiUrl', customMapboxApiUrl);
     return new MapboxGl.Map(options);
+  }
+
+  private assign(obj: any, prop: any, value: any) {
+    if (typeof prop === 'string') {
+      // tslint:disable-next-line:no-parameter-reassignment
+      prop = prop.split('.');
+    }
+    if (prop.length > 1) {
+      const e = prop.shift();
+      this.assign(obj[e] =
+        Object.prototype.toString.call(obj[e]) === '[object Object]'
+          ? obj[e]
+          : {},
+        prop,
+        value);
+    } else {
+      obj[prop[0]] = value;
+    }
   }
 }
