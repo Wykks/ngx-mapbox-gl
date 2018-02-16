74d3886d9362d355fed3d90a99c4a6db13c813bc
Paul Ryan <paul@simplycomplex.co>
2018-02-16 08:52:55 -0500

    feat(public_api): Expose MapComponent

--- src/app/lib/index.ts
+++ src/app/lib/index.ts
@@ -2,5 +2,6 @@ export * from './lib.module';
 
 // Expose MapService for ngx-mapbox-gl extensions
 export * from './map/map.service';
+export * from './map/map.component';
 
 export * from './map/map.types';
