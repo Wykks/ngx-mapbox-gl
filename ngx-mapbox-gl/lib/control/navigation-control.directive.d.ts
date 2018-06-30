206fd48af63fd5b90862172d1dbfa5325bf87396
Wykks <wykksoss@gmail.com>
2018-06-30 16:42:39 +0200

    Auto-generated commit

--- ngx-mapbox-gl/lib/control/navigation-control.directive.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnInit } from '@angular/core';
-import { MapService } from '../map/map.service';
-import { ControlComponent } from './control.component';
-export declare class NavigationControlDirective implements OnInit {
-    private MapService;
-    private ControlComponent;
-    constructor(MapService: MapService, ControlComponent: ControlComponent);
-    ngOnInit(): void;
-}
