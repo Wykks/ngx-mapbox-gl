458c3f7e7dd2cc83ba4039c2add48d9cdc857935
Raymond Suelzer <rsuelzer@gmail.com>
2018-12-06 05:28:15 -0800

    feat: Export control component, for custom control directives (#79)
    close #78

--- projects/ngx-mapbox-gl/src/public_api.ts
+++ projects/ngx-mapbox-gl/src/public_api.ts
@@ -4,6 +4,9 @@
 
 export * from './lib/ngx-mapbox-gl.module';
 
+// Expose control component to allow custom directives
+export * from './lib/control/control.component';
+
 // Expose MapService for ngx-mapbox-gl extensions
 export * from './lib/map/map.service';
 export * from './lib/map/map.component';
