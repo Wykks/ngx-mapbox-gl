2fd8ac27675abcfd47001f582def84fe8187a362
Dmytro Gokun <dmytro.gokun@gmail.com>
2019-07-19 15:42:51 +0300

    Fix control order when using custom controls alongside built-in controls (#75)

--- projects/ngx-mapbox-gl/src/lib/control/navigation-control.directive.ts
+++ projects/ngx-mapbox-gl/src/lib/control/navigation-control.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Host, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Directive, Host, Input } from '@angular/core';
 import { NavigationControl } from 'mapbox-gl';
 import { MapService } from '../map/map.service';
 import { ControlComponent } from './control.component';
@@ -6,7 +6,7 @@ import { ControlComponent } from './control.component';
 @Directive({
   selector: '[mglNavigation]'
 })
-export class NavigationControlDirective implements OnInit {
+export class NavigationControlDirective implements AfterContentInit {
   /* Init inputs */
   @Input() showCompass?: boolean;
   @Input() showZoom?: boolean;
@@ -16,7 +16,7 @@ export class NavigationControlDirective implements OnInit {
     @Host() private ControlComponent: ControlComponent
   ) { }
 
-  ngOnInit() {
+  ngAfterContentInit() {
     this.MapService.mapCreated$.subscribe(() => {
       if (this.ControlComponent.control) {
         throw new Error('Another control is already set for this control');
