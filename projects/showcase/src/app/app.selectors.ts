887c251cf2bbc1ede362f4b72a3c29407f9f97a6
Dmytro Gokun <dmytro.gokun@gmail.com>
2019-10-10 14:40:26 +0300

    showcase: work around an annoying ngrx warning (https://github.com/ngrx/platform/issues/2116)

--- projects/showcase/src/app/app.selectors.ts
+++ projects/showcase/src/app/app.selectors.ts
@@ -1,10 +1,15 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromDemo from './demo/demo.reducer';
-import { RouterReducerState } from '@ngrx/router-store';
-import { RouterStateUrl } from './app.module';
+import { State } from './app.module';
 
 const getDemoState = createFeatureSelector<fromDemo.State>('demo');
-const getRouterState = createFeatureSelector<RouterReducerState<RouterStateUrl>>('router');
+// TEMP: this line is supposed to be:
+//    const getRouterState = createFeatureSelector<RouterReducerState<RouterStateUrl>>('router');
+//  There is an issue with `createFeatureSelector` though, which causes unnecessary warnings, as described here:
+//    https://github.com/ngrx/platform/issues/2116
+//  This workaround is taken from here:
+//    https://stackoverflow.com/questions/58214913/how-to-silence-the-feature-name-router-does-not-exist-in-the-state-ngrx-r
+const getRouterState = createSelector((state: State) => state.router, value => value);
 
 export const isDemoSidenavOpen = createSelector(
   getDemoState,
