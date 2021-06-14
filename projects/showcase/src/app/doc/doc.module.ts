ea2ff4a19c8a06b1156d80b7c648bc9314ba38ad
Eric Schricker <eric.schricker@adiutabyte.de>
2021-06-14 11:28:29 +0200

    replaced ngx-md with ngx-markdown due to discontinued development

--- projects/showcase/src/app/doc/doc.module.ts
+++ projects/showcase/src/app/doc/doc.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule, Routes } from '@angular/router';
-import { NgxMdModule } from 'ngx-md';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/prism';
 import { LayoutModule } from '../shared/layout/layout.module';
 import { DocComponent } from './doc.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 const DOC_ROUTES: Routes = [
   {
@@ -23,7 +24,8 @@ const DOC_ROUTES: Routes = [
     RouterModule.forChild(DOC_ROUTES),
     FormsModule,
 
-    NgxMdModule.forRoot(),
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient }),
 
     MatFormFieldModule,
     MatSelectModule,
