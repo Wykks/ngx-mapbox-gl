dbfe4e5500562bb07b5ad61fec3b5b983f8de1f0
Philippe May <aprilmay@users.noreply.github.com>
2021-07-06 14:55:48 +0530

    fix: safari fullscreen mode does not work properly #301 (#316) (#9)
    * fix: safari fullscreen mode does not work properly #301 (#316)
    
    * fix: exception when accessing to MapService.getSource (#313)
    
    Co-authored-by: Maks <Nosfit@ukr.net>

--- projects/ngx-maplibre-gl/src/lib/control/fullscreen-control.directive.ts
+++ projects/ngx-maplibre-gl/src/lib/control/fullscreen-control.directive.ts
@@ -1,4 +1,10 @@
-import { AfterContentInit, Directive, Host, Input } from '@angular/core';
+import {
+  AfterContentInit,
+  Directive,
+  Host,
+  HostListener,
+  Input,
+} from '@angular/core';
 import { FullscreenControl } from 'maplibre-gl';
 import { MapService } from '../map/map.service';
 import { ControlComponent } from './control.component';
@@ -9,6 +15,10 @@ import { ControlComponent } from './control.component';
 export class FullscreenControlDirective implements AfterContentInit {
   /* Init inputs */
   @Input() container?: HTMLElement;
+  @HostListener('window:webkitfullscreenchange', ['$event.target'])
+  onFullscreen() {
+    this.MapService.mapInstance.resize();
+  }
 
   constructor(
     private MapService: MapService,
