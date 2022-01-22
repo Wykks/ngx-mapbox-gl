df25ab0cb5dc1ba5d9037ea638dfed4ef15ef60c
HarelM <harel.mazor@gmail.com>
2022-01-22 15:07:54 +0200

    Fix satellite component

--- projects/showcase/src/app/demo/examples/satellite-map.component.ts
+++ projects/showcase/src/app/demo/examples/satellite-map.component.ts
@@ -4,9 +4,32 @@ import { Component } from '@angular/core';
   selector: 'showcase-demo',
   template: `
     <mgl-map
-      [style]="
-        'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
-      "
+      [style]="{
+        version: 8,
+        sources: {
+          satellite: {
+            tileSize: 512,
+            type: 'raster',
+            url:
+              'https://api.maptiler.com/tiles/satellite-v2/tiles.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
+          }
+        },
+        layers: [
+          {
+            id: 'satellite',
+            layout: {
+              visibility: 'visible'
+            },
+            minzoom: 0,
+            paint: {
+              'raster-opacity': 1
+            },
+            'raster-opacity': 1,
+            source: 'satellite',
+            type: 'raster'
+          }
+        ]
+      }"
       [zoom]="[9]"
       [center]="[137.9150899566626, 36.25956997955441]"
     >
