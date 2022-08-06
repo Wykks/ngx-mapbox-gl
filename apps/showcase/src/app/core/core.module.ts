3a5d4400e9932eb785e1c14d1866c27bbb11cd11
Wykks <contact@wykks.eu>
2022-08-06 17:09:28 +0200

    showcase: change routing to hash routing

--- apps/showcase/src/app/core/core.module.ts
+++ apps/showcase/src/app/core/core.module.ts
@@ -14,7 +14,7 @@ import { APP_ROUTES } from './routes';
 
     MatIconModule,
 
-    RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(APP_ROUTES, { useHash: true }),
     NgxMapboxGLModule.withConfig({
       accessToken:
         'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA',
