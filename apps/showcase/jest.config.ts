18f4efc8dd29ef78846de2068825c2c64cd7a9ae
Wykks <contact@wykks.eu>
2025-04-13 17:22:34 +0200

    build: rework eslint setup and more

--- apps/showcase/jest.config.ts
+++ apps/showcase/jest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
   displayName: 'showcase',
   preset: '../../jest.preset.js',
--- libs/ngx-mapbox-gl/jest.config.ts
+++ libs/ngx-mapbox-gl/jest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
   displayName: 'ngx-mapbox-gl',
   preset: '../../jest.preset.js',
